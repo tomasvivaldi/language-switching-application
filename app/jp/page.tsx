87bc5ea2d50acdad178257d82b752981f31253bf
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2024-05-08 14:45:31 +0530

    Uniformization of page design

--- app/jp/page.tsx
+++ app/jp/page.tsx
@@ -2,7 +2,7 @@ import LanguageSwitcher from "../components/LanguageSwitcher";
 
 export default function JpHome() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-start p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <div className="z-10 w-full max-w-5xl items-start justify-start font-mono text-sm flex flex-col gap-4">
         <LanguageSwitcher />
         <h1 className="writing-mode-vertical-rl text-orientation-upright text-4xl w-full  flex-row-reverse">
